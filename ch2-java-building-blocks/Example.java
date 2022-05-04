bb8d0951335d74fbb7488794ae9239ae5395eb21
Juan Santos <santos.r09@gmail.com>
2022-05-04 08:28:56 -0500

    chapters 2-java-building-blocks and 3-operators

---
+++ ch2-java-building-blocks/Example.java
@@ -0,0 +1,15 @@
+public class Example {
+
+  public static void main (String args[]) {
+    Example example = new Example();
+
+  }
+
+  public void testingPrimitives() {
+    double x = 39.21;
+    float y = 2.1f;
+    // float yf = 2.1; //Required: float, Provided: Double
+    var z = x + y;
+  }
+
+}
